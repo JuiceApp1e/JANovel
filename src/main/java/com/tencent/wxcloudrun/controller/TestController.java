e885da6d04f1f0580377b6373961468c75c727af
ZiguoCheng <2746738323@qq.com>
2022-04-04 18:16:09 +0800

    "test ci"

--- src/main/java/com/tencent/wxcloudrun/controller/TestController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/TestController.java
@@ -1,2 +1,28 @@
-package com.tencent.wxcloudrun.controller;public class TestController {
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.model.Counter;
+import com.tencent.wxcloudrun.service.CounterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    final Logger logger;
+
+    public TestController() {
+        this.logger = LoggerFactory.getLogger(TestController.class);
+    }
+
+    @GetMapping(value = "/test")
+    ApiResponse get() {
+        logger.info("/test get request");
+        return ApiResponse.ok("succeed");
+    }
 }
